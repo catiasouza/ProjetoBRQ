4a66ad2a67b04ef71f9b762a1174f45e01e862ba
Catia Miranda de Souza <catiamiranda@brq.com>
2019-11-18 11:16:42 -0300

    Ajuste de layout para o dropDown

---
+++ ProjetoBRQ/Cadastro/Model/Protocol.swift
@@ -0,0 +1,9 @@
+//
+//  Protocol.swift
+//  ProjetoBRQ
+//
+//  Created by Catia Miranda de Souza on 14/11/19.
+//  Copyright © 2019 Catia Miranda de Souza. All rights reserved.
+//
+
+import Foundation
