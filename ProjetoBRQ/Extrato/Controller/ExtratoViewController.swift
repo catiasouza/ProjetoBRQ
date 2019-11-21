03ca257a7b59c25a651990ab4491be5bc8f6ed53
Marcello <marcellopontes@brq.com>
2019-11-21 10:26:00 -0300

    corrigindo passagem de dados da cadastro e colocando tentativa de validacao de datas no extrato

--- ProjetoBRQ/Extrato/Controller/ExtratoViewController.swift
+++ ProjetoBRQ/Extrato/Controller/ExtratoViewController.swift
@@ -15,12 +15,35 @@ class ExtratoViewController: UIViewController, UITableViewDelegate, UITableViewD
     var listaLancamentos: [Lancamento] = [
         Lancamento(nome: "Mc Donalds", data: "06 NOV", valor: -21.99),
         Lancamento(nome: "Lojas Americanas", data: "06 NOV", valor: -75.50),
-        Lancamento(nome: "Samsung", data: "07 NOV", valor: -8999.99)
+        Lancamento(nome: "Samsung", data: "07 NOV", valor: -8999.99),
+        Lancamento(nome: "Açai Formosa", data: "08 NOV", valor: -24.99),
+        Lancamento(nome: "Deposito Salario", data: "09 NOV", valor: 7500.00),
+        Lancamento(nome: "Kanui", data: "10 NOV", valor: -8.99),
+        Lancamento(nome: "Burguer King", data: "11 NOV", valor: -34.99),
+        Lancamento(nome: "Netshoes", data: "12 NOV", valor: -20.75),
+        Lancamento(nome: "Ali Express", data: "13 NOV", valor: -122.97),
+        Lancamento(nome: "Tranferencia Itau", data: "14 NOV", valor: 21900.00),
+        Lancamento(nome: "Estacionamento XPress", data: "15 NOV", valor: -5.00),
+        Lancamento(nome: "Dell", data: "15 NOV", valor: -9999.99),
+        Lancamento(nome: "Mc Donalds", data: "15 NOV", valor: -15.40),
+        Lancamento(nome: "Japan House", data: "16 NOV", valor: -1.50),
+        Lancamento(nome: "Carrefour", data: "16 NOV", valor: -25.99),
+        Lancamento(nome: "Honda", data: "17 NOV", valor: -23.99),
+        Lancamento(nome: "Wish", data: "17 NOV", valor: -13.50),
+        Lancamento(nome: "Gearbest", data: "17 NOV", valor: -11.99),
+        Lancamento(nome: "Banggod", data: "18 NOV", valor: -32.99),
+        Lancamento(nome: "Lojas Americanas", data: "18 NOV", valor: -7.50),
+        Lancamento(nome: "Chocolates Brasil", data: "18 NOV", valor: -9.99),
+        Lancamento(nome: "Miniso", data: "19 NOV", valor: -1.99),
+        Lancamento(nome: "Copias LTDA", data: "19 NOV", valor: -0.50),
+        Lancamento(nome: "Fabrica de Bolos", data: "19 NOV", valor: -24.99),
     ]
     var saldoTotal: Double = 0
     // dados recebidos da listaConta
     var apelidoRecebido: String?
     var id:Int?
+    
+    let validador: Bool?
     
     //MARK: - Outlets
     
@@ -62,7 +85,6 @@ class ExtratoViewController: UIViewController, UITableViewDelegate, UITableViewD
         viewExtrato.layer.cornerRadius = 8
         labelExtratoApelido.text = apelidoRecebido
         
-        
         for i in listaLancamentos {
             let valorLista = i
             saldoTotal += valorLista.valor
@@ -81,15 +103,35 @@ class ExtratoViewController: UIViewController, UITableViewDelegate, UITableViewD
         self.textDataFim.text = formatador.string(from: sender.date)
     }
     
+    func arredondaDouble(valor: Double)-> Double{
+        let formato = String(2)+"f"
+        return Double(String(format: "%."+formato, valor))!
+    }
+    
     func validaRangeDatas() -> Bool{
-        guard let dataInicio = textDataInicio.text as? Date else {return false}
-        guard let dataFim = textDataFim.text as? Date else {return false}
         
-        if dataInicio <= dataFim{
-            return true
-        }else{
-            return false
-        }
+        //pegando datas dadas pelo usuario
+        guard let dataInicioString = textDataInicio.text else {return false}
+        guard let dataFimString = textDataFim.text else {return false}
+        
+        //criando formato padrao
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        //convertendo a string das datas para date
+        let dataInicioDate = dateFormatter.date(from: dataInicioString)
+        let dataFimDate = dateFormatter.date(from: dataFimString)
+        
+        //tirando dado do estado de opcional
+        guard let dataInicio = dataInicioDate else{return false}
+        guard let dataFim = dataFimDate else{return false}
+        
+        //        if dataInicio <= dataFim {
+        //            return true
+        //        }else{
+        //            return false
+        //        }
+        return true
     }
     
     //MARK: - TableView
@@ -99,20 +141,34 @@ class ExtratoViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listaLancamentos.count
+        
+        let validador = validaRangeDatas()
+        
+        if validador{
+            return listaLancamentos.count
+        }else{
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celulaExtrato = tableView.dequeueReusableCell(withIdentifier: "celulaExtrato", for: indexPath) as! ExtratoTableViewCell
-        
         let lancamentoAtual = listaLancamentos[indexPath.row]
         
-        celulaExtrato.labelLancamentos.text = lancamentoAtual.nome
-        celulaExtrato.labelDatas.text = lancamentoAtual.data
-        celulaExtrato.labelValores.text = "R$ \(String(describing: lancamentoAtual.valor))"
-        self.somaSaldos.text = "R$ \(String(describing: saldoTotal))"
-                
-        return celulaExtrato
+        if validador{
+            celulaExtrato.labelLancamentos.text = lancamentoAtual.nome
+            celulaExtrato.labelDatas.text = lancamentoAtual.data
+            celulaExtrato.labelValores.text = "R$ \(String(describing: lancamentoAtual.valor))"
+            self.somaSaldos.text = "R$ \(arredondaDouble(valor: saldoTotal))"
+            return celulaExtrato
+        }else{
+            celulaExtrato.labelLancamentos.text = ""
+            celulaExtrato.labelDatas.text = ""
+            celulaExtrato.labelValores.text = ""
+            self.somaSaldos.text = "R$ 0.00"
+            return celulaExtrato
+        }
+        
     }
     
     // MARK: - Teclado
@@ -126,3 +182,4 @@ class ExtratoViewController: UIViewController, UITableViewDelegate, UITableViewD
         view.endEditing(true)
     }
 }
+
