d832cf8d940a86f75c9f8718cedffd6626823ad3
Marcello <marcellopontes@brq.com>
2019-12-06 16:25:12 -0300

    refatorando metodos extrato

---
+++ ProjetoBRQ/Extrato/Model/SetupModel.swift
@@ -0,0 +1,13 @@
+//
+//  SetupModel.swift
+//  ProjetoBRQ
+//
+//  Created by Marcello Pontes Domingos on 06/12/19.
+//  Copyright © 2019 Catia Miranda de Souza. All rights reserved.
+//
+
+import UIKit
+
+class SetupModel: NSObject {
+
+}
