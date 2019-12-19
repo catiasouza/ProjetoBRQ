1b8bbc937314f32425b6ef4bde142574a9665f6f
Matheus Rodrigues Araujo <matheusaraujo@NBBV016619.BRQ.COM>
2019-12-19 15:02:18 -0300

    refatorando listaConta e adicionando ordenacao na listagem

--- ProjetoBRQ/ListaConta/Controller/ListaContaViewController.swift
+++ ProjetoBRQ/ListaConta/Controller/ListaContaViewController.swift
@@ -6,18 +6,15 @@
 //  Copyright © 2019 Catia Miranda de Souza. All rights reserved.
 //
 
-// Projeto oficial que vai subir pro git
-
 import UIKit
 import CoreData
 
-class ListaContaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, ContaDelegate {
+class ListaContaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     //MARK: - Outlets
     
     @IBOutlet weak var collectionListaContas: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var imageLogoBRQ: UIImageView!
     @IBOutlet weak var labelSaldoTotal: UILabel!
     @IBOutlet weak var buttonAdicionar: UIButton!
@@ -25,7 +22,6 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
     //MARK: - Variaveis
     
     var fetchResultController:NSFetchedResultsController<ContaCD>!
-    
     var somaSaldos:Double = 0
     var arraySaldos: [Double] = []
     
@@ -33,7 +29,7 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setAcessibility()
+        ConfiguraListaConta().setAcessibility(imageLogoBRQ: imageLogoBRQ, labelSaldoTotal: labelSaldoTotal, buttonAdicionar: buttonAdicionar)
         collectionListaContas.dataSource = self
         collectionListaContas.delegate = self
         searchBar.delegate = self
@@ -42,11 +38,7 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
     
     override func viewWillAppear(_ animated: Bool) {
         recuperaContas()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        somaSaldos = somaSaldosMetodo(saldosArray: arraySaldos)
-        labelSaldoTotal.text = "R$ \( SetupModel().formatarValor(valor: somaSaldos) )"
+        somaSaldosMetodo(contas: fetchResultController.fetchedObjects!)
     }
     
     //MARK: - Métodos
@@ -62,7 +54,6 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
 
                     let conta = self.fetchResultController.fetchedObjects?[row]
                     self.context.delete(conta!)
-
                     self.recuperaContas()
                     self.collectionListaContas.reloadData()
                 }
@@ -76,70 +67,30 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
     
     func recuperaContas(filtro: String = "") {
         let fetchRequest:NSFetchRequest<ContaCD>  = ContaCD.fetchRequest()
-        fetchRequest.sortDescriptors = []
-        
+        let sort = NSSortDescriptor(key: "data", ascending: false)
+        fetchRequest.sortDescriptors = [sort]
         if filtro != ""{
             let predicate = NSPredicate(format: "apelidoConta contains [c]%@", filtro)
             fetchRequest.predicate = predicate
         }
-        
         fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         fetchResultController.delegate = self
         do {
             try fetchResultController.performFetch()
-            arraySaldos = recuperaSaldo()
-            somaSaldos = somaSaldosMetodo(saldosArray: arraySaldos)
-            labelSaldoTotal.text = "R$ \( SetupModel().formatarValor(valor: somaSaldos) )"
+            somaSaldosMetodo(contas: fetchResultController.fetchedObjects!)
         } catch  {
             print(error.localizedDescription)
         }
     }
     
-    func recuperaSaldo() -> [Double] {
-        somaSaldos = 0
-        var saldos:[Double] = []
-        let n = fetchResultController.fetchedObjects!.count
-        if n != 0 {
-            for i in (0..<n) {
-                // pegar saldo
-                guard let conta = fetchResultController.fetchedObjects?[i] else {return []}
-                let saldo = conta.saldo
-                saldos.append(saldo)
-            }
-            return saldos
-        } else {
-            return []
-        }
-    }
-    
-    private func setAcessibility() {
-        
-        imageLogoBRQ.isAccessibilityElement = true
-        imageLogoBRQ.accessibilityLabel = "Logo da BRQ"
-        imageLogoBRQ.accessibilityTraits = .image
-
-        labelSaldoTotal.isAccessibilityElement = true
-        labelSaldoTotal.accessibilityLabel = "Soma do saldo das contas em Reais"
-        labelSaldoTotal.accessibilityTraits = .none
-        
-        buttonAdicionar.isAccessibilityElement = true
-        buttonAdicionar.accessibilityLabel = "Botão para adicionar nova conta"
-        buttonAdicionar.accessibilityTraits = .button
-    }
-    
-    func somaSaldosMetodo(saldosArray:[Double]) -> Double{
+    func somaSaldosMetodo(contas:[ContaCD]){
         var saldoTotal:Double = 0
-        for saldo in saldosArray {
-            saldoTotal += saldo
+        for conta in contas {
+            saldoTotal += conta.saldo
         }
-        return saldoTotal
+        self.somaSaldos = saldoTotal
+        labelSaldoTotal.text = "R$ \( SetupModel().formatarValor(valor: somaSaldos) )"
     }
-    
-    //MARK: - Delegate
-    
-    func adicionaConta(conta:Conta) {
-    }
-    
     
     //MARK: - CollectionView
     
@@ -154,47 +105,30 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if fetchResultController.fetchedObjects?.count == 0 {
-            self.somaSaldos = 0
-            labelSaldoTotal.text = "R$ \( SetupModel().formatarValor(valor: somaSaldos) )"
             let celula = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPadrao", for: indexPath) as! ContaCollectionViewCell
             
             let mensagem = "Nenhuma conta encontrada, cadastre uma nova conta"
             celula.dadosDaConta(apelido: mensagem, saldo: nil)
             celula.fixaLabels(labelFixaBanco: "", labelFixaAgencia: "", labelFixaConta: "", labelFixaSaldo: "")
-
             celula.configuraExibicaoCelula(celula: celula)
 
             return celula
         } else {
             
             let celula = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPadrao", for: indexPath) as! ContaCollectionViewCell
-            
             let contaSelecionada = fetchResultController.fetchedObjects?[indexPath.row]
-            
-            let apelido = contaSelecionada?.apelidoConta as! String
-            let banco = contaSelecionada?.banco as! String
-            let agencia = String(describing: contaSelecionada!.agencia)
-            let contaNumero = "\(String(describing: contaSelecionada!.conta))" + "-" + "\(String(describing: contaSelecionada!.digito))"
-            let saldo = SetupModel().arredondaDouble(valor: contaSelecionada!.saldo)
-            
-            celula.dadosDaConta(apelido: apelido, banco: banco, agencia: agencia, conta: contaNumero, saldo: saldo)
-            celula.fixaLabels()
-            celula.setAccessibility()
+            ConfiguraListaConta().populaCollection(contaSelecionada: contaSelecionada!, celula: celula, indexPath: indexPath)
             
             let longPress = UILongPressGestureRecognizer(target: self, action: #selector( exibeAlerta ) )
             celula.addGestureRecognizer(longPress)
-            
-            celula.configuraExibicaoCelula(celula: celula)
             
             return celula
         }
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let largura = collectionView.bounds.width
-        let altura: CGFloat = 160
-        return CGSize(width: largura, height: altura)
+
+        return CGSize(width: collectionView.bounds.width, height: 160)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -210,44 +144,20 @@ class ListaContaViewController: UIViewController, UICollectionViewDataSource, UI
             return
         }
     }
-    
-
     //MARK: - Navegação
 
     @IBAction func botaoAdicionar(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "cadastroID") as! CadastroViewController
-        controller.delegate = self
         present(controller, animated: true, completion: nil)
     }
-    
     //MARK: - Search Bar
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         recuperaContas(filtro: searchText )
         collectionListaContas.reloadData()
     }
-    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         self.searchBar.endEditing(true)
-    }
-}
-
-//MARK: - Extensoes
-
-extension ListaContaViewController: NSFetchedResultsControllerDelegate {
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        
-        switch type {
-        case .delete:
-            do {
-                try context.save()
-            } catch  {
-                print(error.localizedDescription)
-            }
-        default:
-            collectionListaContas.reloadData()
-        }
     }
 }
